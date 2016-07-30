b9f28ac258a3baecdedaae331720ebf77cdf6401
java-expert <jagjotmca@yahoo.co.in>
2016-07-30 23:46:17 +0530

    first eclipselink commit

---
+++ src/model/Emp.java
@@ -0,0 +1,36 @@
+package model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Emp {
+
+@Id
+@GeneratedValue(strategy= GenerationType.AUTO)
+private int eid;
+public int getEid() {
+	return eid;
+}
+public void setEid(int eid) {
+	this.eid = eid;
+}
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+public double getSalary() {
+	return salary;
+}
+public void setSalary(double salary) {
+	this.salary = salary;
+}
+private String ename;
+private double salary;
+
+}
