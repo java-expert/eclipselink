c8187408a92723620d843bb7ad3c32487548bee8
java-expert <jagjotmca@yahoo.co.in>
2016-08-28 14:52:29 +0530

    unit of work

--- src/UnitOfWorkTest.java
+++ src/UnitOfWorkTest.java
@@ -5,20 +5,30 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
+import org.eclipse.persistence.internal.jpa.EntityManagerImpl;
+import org.eclipse.persistence.sessions.Session;
+import org.eclipse.persistence.sessions.UnitOfWork;
+import org.eclipse.persistence.sessions.factories.SessionFactory;
+import org.eclipse.persistence.sessions.factories.SessionManager;
+import org.eclipse.persistence.sessions.server.Server;
+
 import model.Emp;
 
 
 public class UnitOfWorkTest {
 	public static void main(String[] args) {
 		try{
-		EntityManagerFactory emfactory = Persistence.
-				createEntityManagerFactory( "EclipseLink" );
-				EntityManager entitymanager = emfactory.
-				createEntityManager( );
-				
-				
-				
-			
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "persistence_unit_name" );
+		EntityManager entityManager = emfactory.createEntityManager( );
+		Session session = ((EntityManagerImpl) entityManager).getActiveSession();
+		entityManager.getTransaction( ).begin( );
+		UnitOfWork uow = session.acquireUnitOfWork();
+		Emp emp = entityManager.find(Emp.class, 1202);
+		uow.registerObject(emp);
+		emp.setEname( "first uow" );
+		emp.setSalary( 50000 );	
+		uow.commit();		
+		entityManager.getTransaction( ).commit();
 		}catch(Exception e){
 			System.out.println(e);
 		}
