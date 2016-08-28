a24c6a5fd5dc2026876e2dc61b90f224b83bbf11
java-expert <jagjotmca@yahoo.co.in>
2016-08-28 14:57:49 +0530

    unit of work batch

--- src/UnitOfWorkBatchTest.java
+++ src/UnitOfWorkBatchTest.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -16,26 +15,18 @@ import org.eclipse.persistence.sessions.server.Server;
 import model.Emp;
 
 
-public class UnitOfWorkTest {
+public class UnitOfWorkBatchTest {
 	public static void main(String[] args) {
 		try{
-		List<Emp> lst=new ArrayList<Emp>();
 		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "persistence_unit_name" );
 		EntityManager entityManager = emfactory.createEntityManager( );
 		Session session = ((EntityManagerImpl) entityManager).getActiveSession();
 		entityManager.getTransaction( ).begin( );
 		UnitOfWork uow = session.acquireUnitOfWork();
 		Emp emp = entityManager.find(Emp.class, 1202);
-		emp.setEname( "3333333333333333333333" );
+		uow.registerObject(emp);
+		emp.setEname( "first uow" );
 		emp.setSalary( 50000 );	
-		
-		Emp emp1 = entityManager.find(Emp.class, 1203);
-		emp1.setEname( "4444444444444444" );
-		emp1.setEid(1);
-		emp1.setSalary( 50000 );	
-		lst.add(emp);
-		lst.add(emp1);
-		uow.registerAllObjects(lst);
 		uow.commit();		
 		entityManager.getTransaction( ).commit();
 		}catch(Exception e){
